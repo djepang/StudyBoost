167a37fdcef7580235ac1cd03dc1c2430eddc9b8
Svitlana Popova <diduhdiduh367@gmail.com>
2024-05-23 01:15:53 +0200

    app rote erstelen

---
+++ Frontend/studyboost/src/StartPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Body from './Body';
+import Category3DGraphic from './Category3DGraphic';
+
+const StartPage = () => {
+    return (
+        <div>
+            <Body />
+            <Category3DGraphic />
+        </div>
+    );
+}
+
+export default StartPage;
