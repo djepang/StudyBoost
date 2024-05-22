c12eedebd975714c534e7582964a0dd26c2ac7e8
Svitlana Popova <diduhdiduh367@gmail.com>
2024-05-22 09:25:56 +0200

    logihpage erstelen

---
+++ Frontend/studyboost/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePage from './HomePage';
+import FutureComponent from './FutureComponent'
+
+const App = () => {
+    return (
+        <div>
+            <HomePage />
+            <hr />
+            <FutureComponent />
+        </div>
+    );
+};
+
+export default App;
