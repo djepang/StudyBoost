60b2ab53262f0e934504a43739949c3be2f34064
Svitlana Popova <diduhdiduh367@gmail.com>
2024-05-24 10:50:06 +0200

    ornungder frontend

---
+++ Frontend/studyboost/src/components/Category3DGraphic.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Category3DGraphic = () => {
+    // Hier können Sie die Integration der 3D-Grafik implementieren
+    return (
+        <div>
+            {/* Hier wird die 3D-Grafik angezeigt */}
+            <canvas id="categoryCanvas"></canvas>
+        </div>
+    );
+}
+
+export default Category3DGraphic;
