cc0255a140c66309052af5f03f575511ce8ce20e
Svitlana Popova <diduhdiduh367@gmail.com>
2024-06-13 22:33:59 +0200

    linkend profil erstelen

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -7,13 +7,13 @@ class Footer extends React.Component {
             <footer className="p-3 bg-gray-800 text-white text-center">
                 <p>&copy; 2024 StudyBoost</p>
                 <div className="flex justify-center space-x-4">
-                    <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="text-lg text-blue-600 font-bold">
+                    <a href="https://www.linkedin.com/in/studyboost-studyboost-55ba07313" target="_blank" rel="noopener noreferrer" className="text-lg text-blue-600 font-bold">
                         <FaLinkedin className="text-blue-600 inline-block" />
                     </a>
-                    <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-lg text-blue-600 font-bold">
+                    <a href="https://twitter.com/StudyBoostprofile" target="_blank" rel="noopener noreferrer" className="text-lg text-blue-600 font-bold">
                         <FaTwitter className="text-blue-600 inline-block" />
                     </a>
-                    <a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-lg text-blue-600 font-bold">
+                    <a href="https://facebook.com/StudyBoostprofile" target="_blank" rel="noopener noreferrer" className="text-lg text-blue-600 font-bold">
                         <FaFacebook className="text-blue-600 inline-block" />
                     </a>
                 </div>
