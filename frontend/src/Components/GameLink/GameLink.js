c15b5ae9be6f565d53b1e4283fed66136c29cf1d
Svitlana Popova <diduhdiduh367@gmail.com>
2024-06-18 12:24:05 +0200

    google play button erstelen

--- frontend/src/Components/GameLink/GameLink.js
+++ frontend/src/Components/GameLink/GameLink.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import './GameLink.css'; // Import der CSS-Datei
+import googlePlayLogo from '../../assets/google-play-logo.png'; // Import des Google Play-Logos
 
 function GameLink() {
+  const handleClick = () => {
+    window.open('https://play.google.com/store/apps/details?id=com.rvappstudios.math.kids.counting', '_blank');
+  };
+
   return (
     <div className="game-link-container">
       <h2><strong>Mathematik-Spiel für Kinder</strong></h2>
-      <a 
-        href="https://play.google.com/store/apps/details?id=com.rvappstudios.math.kids.counting"
-        target="_blank"
-        rel="noopener noreferrer"
-      ><strong>
-         Bitte Spiel im   Google Play Store herunterladen
-         
-        </strong></a>
+      <button 
+        onClick={handleClick}
+        className="google-play-button"
+      >
+        <img src={googlePlayLogo} alt="Google Play Logo" className="google-play-logo" /></button>
+        <span><strong>Bitte spiel im Google Play Store herunterladen. Danke!</strong></span>
+      
     </div>
   );
 }
 
 export default GameLink;
+
