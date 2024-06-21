b3759111808eb6911feaed6dc2a5b63dc72958de
Svitlana Popova <diduhdiduh367@gmail.com>
2024-06-21 13:41:33 +0200

    header und footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', 
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
