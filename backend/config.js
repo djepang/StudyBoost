ab0599ab3a6dd7929e2910f05229333334420afe
Armel <adjepang@yahoo.fr>
2024-06-26 11:00:16 +0200

    aufraumenbackend

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGO_URI: process.env.MONGO_URI, // Verwenden Sie die MONGO_URI-Umgebungsvariable
-  JWT_SECRET: process.env.JWT_SECRET // Verwenden Sie die SECRET_KEY-Umgebungsvariable
-};
