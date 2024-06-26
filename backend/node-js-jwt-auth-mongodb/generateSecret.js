0e808047f65b6cf650114335ab8fe53a7d339928
Svitlana Popova <diduhdiduh367@gmail.com>
2024-06-26 11:12:13 +0200

    aufgereum

--- backend/node-js-jwt-auth-mongodb/generateSecret.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const generateSecret = (length) => {
-  return crypto.randomBytes(length).toString('hex');
-};
-
-const secret = generateSecret(32); // Generieren einer 32-Byte-Zeichenfolge (64 Zeichen in Hex)
-console.log(secret);
